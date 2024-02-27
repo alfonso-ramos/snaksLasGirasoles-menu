33e51c20e6cc59ff845a79350b09be166ccc5f2b
Alfonso <alfonsoramos.1803@gmail.com>
2024-02-26 21:00:39 -0700

    [ADD] Creados los componentes de botones de telefono y whatsapp y snackcard, creando un mini db con un JSON con los snacks, agregado un sugar for loop en la vista de menu para mostrar los snacks

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,3 +1,68 @@
-export const db = [
-    
+export const snacks = [
+    {
+        titulo: 'Elote entero',
+        descripcion: 'Elote en palo natural con queso y crema o mayonesa',
+        precio: 30,
+        categoria: 'elote',
+        img: '/home/poncho/Documentos/projects/snaksLasGirasoles-menu/src/assets/img/logo.png'
+    },
+    {
+        titulo: 'Elote en vaso',
+        descripcion: 'Elote desgranado en vaso',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Mixto',
+        descripcion: 'Charola con elote entero, sabritas al rededor y elote drafranado encima',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'DuoMix',
+        descripcion: 'Litro de elote desfranado y unas sabritas enteras con elote partido en dos por encima ',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Envuelto',
+        descripcion: 'Elote entero con queso y crema o mayonesa, envuelto con sabritas molidas de su eleccion',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Envuelto especial',
+        descripcion: 'Elote entero envuelto con la sabrita de su preferencia, sabritas enteras alrededor y elote desgranado',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Elote erupcion',
+        descripcion: 'Plato de elote desgranado con sabritas enteras, al centro un medio litro con dos mitades de elote entero clavados con queso, crema y queso amarillo',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Elección',
+        descripcion: 'Medio litro de elote desgranado con una sabrita molida, queso, crema o mayonesa y queso amarillo',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Eleccion especial',
+        descripcion: 'Charola de elote con una elección normal en el medio, al rededor una sabrita entera con elote desgranado',
+        precio: 30,
+        categoria: 'elote',
+    },
+    {
+        titulo: 'Esquisopa',
+        descripcion: 'Charola con una maruchan preparado con elote desgranado y una sabrita enteras',
+        precio: 30,
+        categoria: 'elote',
+    },
+    //TODO Agridulce
+    //TODO tostitos
+    //TODO Helado
+    //TODO Caliente
+    //TODO Bebidas
 ]
