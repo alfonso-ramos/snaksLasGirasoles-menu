48df5692c2531e7d4707f202080123152c9041e3
Alfonso <alfonsoramos.1803@gmail.com>
2024-03-06 21:05:02 -0700

    [ADD] Se agrego la logica para que al momento de hacer click sobre la categoria se cree el array solamente con los snacks de esa categoria

--- src/utils/FilterCategory.js
+++ src/utils/FilterCategory.js
@@ -1,17 +1,18 @@
 import {snacks} from '../data/menu.js'
     
+
 export let snacksFilter = []
 export const filterCategory = (category) => {
+
     if (category === null ){
         snacksFilter.push(...snacks)
+        
     } else {
         snacksFilter = []
-        snacks.forEach(snack => {
-            if(snack.categoria === category) {
-                snacksFilter.push(snack)
-            }
-        })
+        const newFilter = snacks.filter(snack => snack.categoria === category)
+        snacksFilter.push(...newFilter)
+
     }
+    console.log(snacksFilter)
     return snacksFilter
 }
-filterCategory(null)
