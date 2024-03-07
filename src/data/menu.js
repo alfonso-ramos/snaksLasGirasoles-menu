47328310f3dc31967b39f68dc11026b17bc81443
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-03-07 09:25:00 -0700

    [ADD] Agregos los id en la db

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,5 +1,5 @@
 export const snacks = [
-    {
+    { 
         id: 1,
         titulo: 'Elote entero',
         descripcion: 'Elote en palo natural con queso y crema o mayonesa',
@@ -7,14 +7,16 @@ export const snacks = [
         categoria: 'Elote',
         img: '/src/assets/img/entero.webp'
     },
-    {
+    { 
+        id: 2,
         titulo: 'Elote en vaso',
         descripcion: 'Elote desgranado en vaso',
         precio: 25,
         categoria: 'Elote',
         img: '/src/assets/img/eloteVaso.webp'
     },
-    {
+    { 
+        id: 3,
         titulo: 'Mixto',
         descripcion: 'Charola con elote entero, sabritas al rededor y elote drafranado encima',
         precio: 105,
@@ -22,290 +24,335 @@ export const snacks = [
         img: '/src/assets/img/eloteMixto.webp'
 
     },
-    {
+    { 
+        id: 4,
         titulo: 'DuoMix',
         descripcion: 'Litro de elote desfranado y unas sabritas enteras con elote partido en dos por encima ',
         precio: 105,
         categoria: 'Elote',
         img: '/src/assets/img/Duomiox.webp'
     },
-    {
+    { 
+        id: 5,
         titulo: 'Envuelto',
         descripcion: 'Elote entero con queso y crema o mayonesa, envuelto con sabritas molidas de su eleccion',
         precio: 60,
         categoria: 'Elote',
         img: '/src/assets/img/envuelto.webp'
     },
-    {
+    { 
+        id: 6,
         titulo: 'Envuelto especial',
         descripcion: 'Elote entero envuelto con la sabrita de su preferencia, sabritas enteras alrededor y elote desgranado',
         precio: 120,
         categoria: 'Elote',
     },
-    {
+    { 
+        id: 7,
         titulo: 'Elote erupcion',
         descripcion: 'Plato de elote desgranado con sabritas enteras, al centro un medio litro con dos mitades de elote entero clavados con queso, crema y queso amarillo',
         precio: 120,
         categoria: 'Elote',
         img: '/src/assets/img/eloteErupcion.webp'
     },
-    {
+    { 
+        id: 8,
         titulo: 'Elección',
         descripcion: 'Medio litro de elote desgranado con una sabrita molida, queso, crema o mayonesa y queso amarillo',
         precio: 70,
         categoria: 'Elote',
         img: '/src/assets/img/eleccionNormal.webp'
     },
-    {
+    { 
+        id: 9,
         titulo: 'Eleccion especial',
         descripcion: 'Charola de elote con una elección normal en el medio, al rededor una sabrita entera con elote desgranado',
         precio: 105,
         categoria: 'Elote',
         img: '/src/assets/img/eleccionEspecial.webp'
     },
-    {
+    { 
+        id: 10,
         titulo: 'Esquisopa',
         descripcion: 'Charola con una maruchan preparado con elote desgranado y una sabrita enteras',
         precio: 105,
         categoria: 'Elote',
         img: '/src/assets/img/esquisopa.webp'
     },
-    {
+    { 
+        id: 11,
         titulo: 'Cueritos',
         descripcion: 'Cueritos preparados con tomate, cebolla y pepino, sal y pimienta',
         precio: 45,
         categoria: 'Agridulce',
         img: '/src/assets/img/CueritosPreparados.webp'
     },
-    {
+    { 
+        id: 12,
         titulo: 'Giracrazy',
         descripcion: 'Charola con una sabrita entera, zanahoria rayada, cueritos, salchicha, pepino, jicama, cacahuates, rielitos y una variedad de gomitas',
         precio: 105,
         categoria: 'Agridulce',
         img: '/src/assets/img/giracrazy.webp'
     },
-    {
+    { 
+        id: 13,
         titulo: 'Cacahuatada',
         descripcion: 'Vaso con cacahuates, cueritos, pepino, jicama, rielitos, kermato y salsa agridulce',
         precio: 40,
         categoria: 'Agridulce',
         img: '/src/assets/img/cacahuatada.webp'
     },
-    {
+    { 
+        id: 14,
         titulo: 'Takisloco',
         descripcion: 'Takis preparado con ceviche, pepino, jicama, cacahuates, rielitos y gomitas',
         precio: 80,
         categoria: 'Agridulce',
     },
-    {
+    { 
+        id: 15,
         titulo: 'Manzana',
         descripcion: 'Manzana enchilada preparada con pepino, jicama, cacahuates, rielitos, gomitas y salsa agridulce',
         precio: 70,
         categoria: 'Agridulce',
     },
-    {
+    { 
+        id: 16,
         titulo: 'Churroloco',
         descripcion: 'Vaso com churros de maiz, cacahuates, salchichas, pepino, jicama, rielitos, kermaot, gomitas y salsa agridulce',
         precio: 50,
         categoria: 'Agridulce',
     },
-    {
+    { 
+        id: 17,
         titulo: 'Papas preparadas',
         descripcion: 'Papas con ceviche, cueritos, pepino, jicama, cacahuates, rielitos, kermato y salsa agridulce',
         precio: 60,
         categoria: 'Agridulce',
     },
-    {
+    { 
+        id: 18,
         titulo: 'Gomitas preparadas',
         descripcion: 'Gomitas preparadas con chamoy casero de ciruela y salsa agridulce',
         precio: 45,
         categoria: 'Agridulce',
     },
-    {
+    { 
+        id: 19,
         titulo: 'Tostielote',
         descripcion: 'Tostitos con elote desgranado, queso amarillo, queso y crema o mayones',
         precio: 65,
         categoria: 'Tostitos',
         img: '/src/assets/img/Tostielote.webp'
     },
-    {
+    { 
+        id: 20,
         titulo: 'Tostiloco',
         descripcion: 'Tostitos con ceviche de sierra, cueritos, pepino, jicama, cacahuate, rielitos y kermato',
         precio: 65,
         categoria: 'Tostitos',
         img: '/src/assets/img/tostiloco.webp'
     },
-    {
+    { 
+        id: 21,
         titulo: 'Tosticamaron',
         descripcion: 'Tostitos con ceviche de camaron, cueritos, jicama, cacahuates, rielitos y kermato',
         precio: 70,
         categoria: 'Tostitos',
         img: '/src/assets/img/tostiCamaron.webp'
     },
-    {
+    { 
+        id: 22,
         titulo: 'Tosticueritos',
         descripcion: 'Tostitos con cueritos, tomate, pepino, cebolla, cacahuates, rielitos y kermato',
         precio: 65,
         categoria: 'Tostitos',
         img: '/src/assets/img/'
     },
-    {
+    { 
+        id: 23,
         titulo: 'Tostiloco Mixto',
         descripcion: '',
         precio: 120,
         categoria: 'Tostitos',
     },
     //TODO Helado
-    {
+    { 
+        id: 24,
         titulo: 'Raspados',
         //TODO aagregar descripciones
         descripcion: '',
         precio: 25,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 25,
         titulo: 'Frappé',
         descripcion: '',
         precio: 70,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 26,
         titulo: 'Bolis de agua',
         descripcion: '',
         precio: 15,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 27,
         titulo: 'Bolis de leche',
         descripcion: '',
         precio: 20,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 28,
         titulo: 'Fresas con crema',
         //TODO aagregar descripciones
         descripcion: '',
         precio: 70,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 29,
         titulo: 'Mango con crema',
         descripcion: '',
         precio: 70,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 30,
         titulo: 'Freman',
         descripcion: '',
         precio: 70,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 31,
         titulo: 'Chamoyada',
         descripcion: '',
         precio: 45,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 32,
         titulo: 'Jamaicada',
         descripcion: '',
         precio: 45,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 33,
         titulo: 'Paragüitas ',
         descripcion: '',
         precio: 25,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 34,
         titulo: 'Mangoneadas',
         descripcion: '',
         precio: 25,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 35,
         titulo: 'Chocobanana ',
         descripcion: '',
         precio: 25,
         categoria: 'Helado',
     },
-    {
+    { 
+        id: 36,
         titulo: 'Chocofresa',
         descripcion: '',
         precio: 25,
         categoria: 'Helado',
     },
     //TODO Caliente
-    {
+    { 
+        id: 37,
         titulo: 'HotCakes',
         descripcion: '',
         precio: 25,
         categoria: 'Caliente',
     },
-    {
+    { 
+        id: 38,
         titulo: 'Platano frito ',
         descripcion: '',
         precio: 30,
         categoria: 'Caliente',
     },
-    {
+    { 
+        id: 39,
         titulo: 'Mini donas',
         descripcion: '',
         precio: 25,
         categoria: 'Caliente',
     },
     //TODO Bebidas
-    {
+    { 
+        id: 40,
         titulo: 'Refrescos de 600ml',
         descripcion: '',
         precio: 20,
         categoria: 'Bebidas',
     },
-    {
+    { 
+        id: 41,
         titulo: 'Agua natural',
         descripcion: '',
         precio: 15,
         categoria: 'Bebidas',
     },
-    {
+    { 
+        id: 42,
         titulo: 'Agua de sabor',
         descripcion: '',
         precio: 25,
         categoria: 'Bebidas',
     },
-    {
+    { 
+        id: 43,
         titulo: 'Cocohorchata',
         descripcion: '',
         precio: 40,
         categoria: 'Bebidas',
     },
     //Todo postres
-    {
+    { 
+        id: 44,
         titulo: 'Pay de Guayaba',
         descripcion: '',
         precio: 60,
         categoria: 'Postres',
     },
-    {
+    { 
+        id: 45,
         titulo: 'Flan',
         descripcion: '',
         precio: 60,
         categoria: 'Postres',
     },
-    {
+    { 
+        id: 46,
         titulo: 'Pastel tres leches',
         descripcion: '',
         precio: 60,
         categoria: 'Postres',
     },
-    {
+    { 
+        id: 47,
         titulo: 'Chocoflan',
         descripcion: '',
         precio: 60,
         categoria: 'Postres',
     },
-    {
+    { 
+        id: 48,
         titulo: 'Cheesecake de Oreo',
         descripcion: '',
         precio: 60,
